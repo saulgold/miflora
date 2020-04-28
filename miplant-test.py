398e6a98971728ed5d788a5cb915467f4d81284c
saulgold <saulgold@gmail.com>
2020-04-28 21:17:22 +0000

    initial website started

--- miplant-test.py
+++ miplant-test.py
@@ -1,5 +1,14 @@
-
 from miplant import MiPlant
 
-for plant in MiPlant.discover():
-        print(plant.temperature)
+sensors = MiPlant.discover(interface_index=0, timeout=5)
+print('Found ' + str(len(sensors)) + ' sensor/s')
+
+for plant in sensors:
+        print('--------------------------')
+        print('Address: %s' % plant.address)
+        print('Battery level: %i%%' % plant.battery)
+        print('Firmware: %s' % plant.firmware)
+        print('Temperature: %.01f °C' % plant.temperature)
+        print('Light: %.0f lx' % plant.light)
+        print('Moisture: %.0f%%' % plant.moisture)
+        print('Conductivity: %.0f µS/cm' % plant.conductivity)
